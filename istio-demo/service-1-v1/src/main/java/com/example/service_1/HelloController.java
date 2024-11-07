42f231ac9791058275f1f2a84a3b5dd21a7671be
sumangcodes <sumangcodes@gmail.com>
2024-11-07 19:44:03 +0530

    traffic splitting based on weightage

---
+++ istio-demo/service-1-v1/src/main/java/com/example/service_1/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.service_1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Service-1! - V1 ";
+    }
+    
+    @GetMapping("/service-info")
+    public String serviceInfo() {
+        return "Service-1 -V1  is up and running!";
+    }
+}
